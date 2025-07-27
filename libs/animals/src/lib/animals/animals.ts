fc98d04ca46f6578df383434dd50a5095e0ea50a
Dmitrij Vojtov <dmitrij.vojtov@compeople.de>
2025-07-27 19:56:27 +0200

    zoneless change detection

--- libs/animals/src/lib/animals/animals.ts
+++ libs/animals/src/lib/animals/animals.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'lib-animals',
   imports: [CommonModule],
   templateUrl: './animals.html',
   styleUrl: './animals.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Animals {}
--- libs/dashboard/src/lib/dashboard/dashboard.ts
+++ libs/dashboard/src/lib/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'lib-dashboard',
   imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {}
