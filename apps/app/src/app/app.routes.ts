1e549a59fe6da6e26807ae21b18f43f2eecdf07d
Dmitrij Vojtov <dmitrij.vojtov@compeople.de>
2025-07-27 20:14:02 +0200

    redirecting to dashboard, removed nx component

--- apps/app/src/app/app.routes.ts
+++ apps/app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('@my-favorite-animals/dashboard').then((m) => m.routes),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- libs/dashboard/src/index.ts
+++ libs/dashboard/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/dashboard.routes';
 export * from './lib/dashboard/dashboard';
