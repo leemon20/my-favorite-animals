a06cdab3aa411a52ada7b5c0de6442a32afd0dbf
Dmitrij Vojtov <dmitrij.vojtov@compeople.de>
2025-07-29 15:06:23 +0200

    fixed animals page reference

--- libs/features/animals/animals-ui/src/lib/animals.routes.ts
+++ libs/features/animals/animals-ui/src/lib/animals.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/animals-list/animals-list.component').then((m) => m.AnimalsListComponent),
+    loadComponent: () => import('./pages/animals/animals.page').then((m) => m.AnimalsPage),
     pathMatch: 'full',
   },
 ];
