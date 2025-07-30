4007ed2b99afd5a76aabe4117915df3058126aa5
Dmitrij Vojtov <dmitrij.vojtov@compeople.de>
2025-07-30 22:32:21 +0200

    moved widgets to its own package

--- libs/features/animals/animals-ui/src/lib/widgets/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './animal-of-day-widget/animal-of-day.widget';
-export * from './favorite-animals-widget/favorite-animals.widget';
---
+++ libs/features/animals/animals-widgets/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/animal-of-day-widget/animal-of-day.widget';
+export * from './lib/favorite-animals-widget/favorite-animals.widget';
